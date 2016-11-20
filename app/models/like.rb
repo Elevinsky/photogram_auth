c471bc79696c9c58e671dd04e0d3f799357617f1
Erica Levinsky <elevinsky@gmail.com>
2016-11-19 22:28:17 -0600

    finish up associations

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,7 @@ class Like < ActiveRecord::Base
 
   validates :user_id, :presence => true, :uniqueness => true
   validates :photo_id, :presence => true
+  belongs_to :user
+  belongs_to :photo
   
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,5 +5,6 @@ class Photo < ActiveRecord::Base
     has_many :comments
     has_many :likes
     has_many :fans, :through => :likes, :source => :user
+    belongs_to :user
 
 end
