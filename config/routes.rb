544975cdb94d5c653330ad24794df984c1f6c902
Erica Levinsky <elevinsky@gmail.com>
2016-11-19 22:23:19 -0600

    associations

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-root 'photos#index'
+  root 'photos#index'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120033634) do
+ActiveRecord::Schema.define(version: 20161120035549) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "photo_id"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "photo_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.text     "caption"
+    t.string   "image"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
